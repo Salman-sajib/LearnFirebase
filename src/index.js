46f6a9c2c9414482d6ca3c10286083190d465af2
Salman-sajib <sfsajib@gmail.com>
2024-01-22 23:45:02 +0600

    initialize the project, install node modules, install webpack, add firebase to the project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDDTTrX4x1StMGZhhGxZKgvEx64KXaBQIw',
+  authDomain: 'learn-firebase-49efd.firebaseapp.com',
+  projectId: 'learn-firebase-49efd',
+  storageBucket: 'learn-firebase-49efd.appspot.com',
+  messagingSenderId: '55466744579',
+  appId: '1:55466744579:web:7d914d1f4cb4938120e736',
+};
+
+initializeApp(firebaseConfig);
