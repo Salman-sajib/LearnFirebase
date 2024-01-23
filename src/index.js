eee74bc5086f9927b1659140d294b12374c6a528
Salman-sajib <sfsajib@gmail.com>
2024-01-23 12:49:50 +0600

    added firebase queries

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import {
   addDoc,
   deleteDoc,
   doc,
+  query,
+  where,
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -26,8 +28,11 @@ const db = getFirestore();
 // collection ref
 const colRef = collection(db, 'books');
 
+// queries
+const q = query(colRef, where('author', '==', 'patrick rothfuss'));
+
 // real time collection data
-onSnapshot(colRef, (snapshot) => {
+onSnapshot(q, (snapshot) => {
   let books = [];
   snapshot.docs.forEach((doc) => {
     books.push({ ...doc.data(), id: doc.id });
