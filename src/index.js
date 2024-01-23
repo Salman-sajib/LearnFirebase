3027b5d9b56861128f21a0912fd5ad78cf10028b
Salman-sajib <sfsajib@gmail.com>
2024-01-23 14:39:21 +0600

    ordered data with timestamps

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import {
   doc,
   query,
   where,
+  orderBy,
+  serverTimestamp,
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -29,7 +31,7 @@ const db = getFirestore();
 const colRef = collection(db, 'books');
 
 // queries
-const q = query(colRef, where('author', '==', 'patrick rothfuss'));
+const q = query(colRef, orderBy('createdAt'));
 
 // real time collection data
 onSnapshot(q, (snapshot) => {
@@ -48,6 +50,7 @@ addBookForm.addEventListener('submit', (e) => {
   addDoc(colRef, {
     title: addBookForm.title.value,
     author: addBookForm.author.value,
+    createdAt: serverTimestamp(),
   }).then(() => {
     addBookForm.reset();
   });
